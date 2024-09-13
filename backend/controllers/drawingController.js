5f164f73048104f6f7cb9fbe746231db23283cb3
abu.salman <abu.salman@tirzok.com>
2024-09-14 01:29:39 +0600

    add error handlers middleware and utils

--- backend/controllers/drawingController.js
+++ backend/controllers/drawingController.js
@@ -1,61 +1,79 @@
 const Drawing = require('../models/Drawing');
+const messages = require('../utils/messages');
+const statusCodes = require('../utils/statusCodes');
+const CustomError = require('../utils/CustomError');
 
 // Create a new drawing
-exports.createDrawing = async (req, res) => {
+exports.createDrawing = async (req, res, next) => {
   try {
-    const drawing = new Drawing(req.body); // Use req.body to get the data from the request
+    const drawing = new Drawing(req.body);
     const savedDrawing = await drawing.save();
-    res.status(201).json(savedDrawing);
+    res.status(statusCodes.CREATED).json({
+      success: true,
+      data: savedDrawing,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(new CustomError(statusCodes.BAD_REQUEST, messages.ERROR_SAVING_DRAWING));
   }
 };
 
 // Get all drawings
-exports.getAllDrawings = async (req, res) => {
+exports.getAllDrawings = async (req, res, next) => {
   try {
     const drawings = await Drawing.find();
-    res.status(200).json(drawings);
+    res.status(statusCodes.OK).json({
+      success: true,
+      data: drawings,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(new CustomError(statusCodes.BAD_REQUEST, messages.ERROR_FETCHING_DRAWINGS));
   }
 };
 
 // Get a drawing by ID
-exports.getDrawingById = async (req, res) => {
+exports.getDrawingById = async (req, res, next) => {
   try {
     const drawing = await Drawing.findById(req.params.id);
     if (!drawing) {
-      return res.status(404).json({ message: 'Drawing not found' });
+      return next(new CustomError(statusCodes.NOT_FOUND, messages.DRAWING_NOT_FOUND));
     }
-    res.status(200).json(drawing);
+    res.status(statusCodes.OK).json({
+      success: true,
+      data: drawing,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(new CustomError(statusCodes.BAD_REQUEST, messages.ERROR_FETCHING_DRAWINGS));
   }
 };
 
 // Update a drawing by ID
-exports.updateDrawing = async (req, res) => {
+exports.updateDrawing = async (req, res, next) => {
   try {
     const drawing = await Drawing.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!drawing) {
-      return res.status(404).json({ message: 'Drawing not found' });
+      return next(new CustomError(statusCodes.NOT_FOUND, messages.DRAWING_NOT_FOUND));
     }
-    res.status(200).json(drawing);
+    res.status(statusCodes.OK).json({
+      success: true,
+      data: drawing,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(new CustomError(statusCodes.BAD_REQUEST, messages.ERROR_UPDATING_DRAWING));
   }
 };
 
 // Delete a drawing by ID
-exports.deleteDrawing = async (req, res) => {
+exports.deleteDrawing = async (req, res, next) => {
   try {
     const deletedDrawing = await Drawing.findByIdAndDelete(req.params.id);
     if (!deletedDrawing) {
-      return res.status(404).json({ message: 'Drawing not found' });
+      return next(new CustomError(statusCodes.NOT_FOUND, messages.DRAWING_NOT_FOUND));
     }
-    res.status(200).json({ message: 'Drawing deleted successfully' });
+    res.status(statusCodes.OK).json({
+      success: true,
+      message: messages.DRAWING_DELETED_SUCCESS,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(new CustomError(statusCodes.BAD_REQUEST, messages.ERROR_DELETING_DRAWING));
   }
 };
